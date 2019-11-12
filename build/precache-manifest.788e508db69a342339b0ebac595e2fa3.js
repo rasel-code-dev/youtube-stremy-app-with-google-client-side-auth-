d4c170c4b179d3cc096fde0316a7432605cd7a51
raselmahmudr <raselmr005@gmail.com>
2019-11-12 17:51:03 +0600

    FFFFFFFFFFFFF

---
+++ build/precache-manifest.788e508db69a342339b0ebac595e2fa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae7aa1f3c8b838d674d6364011af7ae",
+    "url": "/react-app1/index.html"
+  },
+  {
+    "revision": "b8c65f63130993b0f4dd",
+    "url": "/react-app1/static/css/main.cf4d7270.chunk.css"
+  },
+  {
+    "revision": "c45155b32cbb5aab978a",
+    "url": "/react-app1/static/js/2.97fa236b.chunk.js"
+  },
+  {
+    "revision": "b8c65f63130993b0f4dd",
+    "url": "/react-app1/static/js/main.217963b7.chunk.js"
+  },
+  {
+    "revision": "f6e1d3f9361bc906373c",
+    "url": "/react-app1/static/js/runtime-main.24e904e9.js"
+  }
+]);
